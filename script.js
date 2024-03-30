52eeab9b6206631bfd81cdc02db4c24723f197a9
DanTCTA <daniil.here@gmail.com>
2024-03-29 22:45:21 -0700

    Improved camera
    - Fixed camera pos and cube pos not changing their actual pos
    - Made the camera able to follow the player
    - Added more smoothness to camera turning

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer();
 const loader = new THREE.TextureLoader();
 
-var fps = 144
-
+var fps = 60
+var maxCameraDist = 10
 var movementSpeed = 16
+var rotationSmoothing = 0.05
+var movementSmoothing = 0.05
 
 var delta = 0
 
 var cubePos = {
     x: 0,
-    y: 10,
+    y: 2,
     z: 0,
     movementSpeed,
     geometry: new THREE.BoxGeometry(2, 4, 2),
@@ -22,7 +24,7 @@ const cube = new THREE.Mesh(cubePos.geometry, cubePos.material);
 
 var cameraPos = {
     x: 0,
-    y: 15,
+    y: 7,
     z: 0,
     movementSpeed
 }
@@ -40,11 +42,11 @@ document.querySelector('canvas').remove();
 scene.background = new THREE.Color('#161718');
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-camera.position.set(0, 15, 0);
+camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z);
 camera.rotation.set(0, 0, 0);
 const light = new THREE.HemisphereLight('#FFFFFF', '#757575', 1.7);
 scene.add(light);
-cube.position.set(0, 0, 0);
+cube.position.set(cubePos.x, cubePos.y, cubePos.z);
 cube.rotation.set(0, 0, 0);
 
 planePos.material.map = planePos.texture;
@@ -58,7 +60,43 @@ var lastMousePos = 0;
 let keysPressed = {};
 setInterval(() => {
     delta = 1/fps
+
+    // Camera smoothing
+    var beforeX = camera.rotation.x
+    var beforeY = camera.rotation.y
+    var beforeZ = camera.rotation.z
     camera.lookAt(cubePos.x, cubePos.y, cubePos.z)
+    
+    // Deals with a jitter when the camera changes between a rotation value of -PI to PI
+    if (Math.abs(camera.rotation.z - beforeZ) >= Math.PI) {
+        if (beforeZ < 0)
+            var rotationZ = beforeZ - (2 * Math.PI - camera.rotation.z + beforeZ) * rotationSmoothing
+        else 
+            var rotationZ = beforeZ + (2 * Math.PI + camera.rotation.z - beforeZ) * rotationSmoothing
+
+        if(Math.abs(rotationZ) > Math.PI) {
+            if (rotationZ < 0)
+                rotationZ = rotationZ + (2 * Math.PI)
+            else 
+                rotationZ = rotationZ - (2 * Math.PI)
+        }
+    }
+    else {
+        var rotationZ = beforeZ + (camera.rotation.z - beforeZ) * rotationSmoothing
+    }
+
+    camera.rotation.set(beforeX + (camera.rotation.x - beforeX) * rotationSmoothing, beforeY + (camera.rotation.y - beforeY) * rotationSmoothing, rotationZ)
+
+    // Camera follow player script
+    var playerDist = Math.sqrt((cubePos.x - cameraPos.x)**2 + (cubePos.y - cameraPos.y)**2 + (cubePos.z - cameraPos.z)**2)
+    if (playerDist > maxCameraDist) {
+        var scale = maxCameraDist / playerDist
+        cameraPos.x = cameraPos.x - (cameraPos.x - (cubePos.x + (cameraPos.x - cubePos.x) * scale)) * movementSmoothing
+        // cameraPos.y = cameraPos.y - (cameraPos.y - (cubePos.y + (cameraPos.y - cubePos.y) * scale)) * movementSmoothing
+        cameraPos.z = cameraPos.z - (cameraPos.z - (cubePos.z + (cameraPos.z - cubePos.z) * scale)) * movementSmoothing
+
+        camera.position.set(cameraPos.x, cameraPos.y, cameraPos.z)
+    }
     if (keysPressed['d']) {
         cubePos.x += cubePos.movementSpeed * delta;
     }
